f8d936998c07f39e8dbd02482c177b8278a8e2e6
ponluxime <dandrebap07@gmail.com>
2026-08-04 14:50:49 -0500

    Made settings window resizable

--- App/Dialog/SettingsDialog.h
+++ App/Dialog/SettingsDialog.h
@@ -10,6 +10,7 @@ public:
 	const char* GetTitle() override { return "Settings"; }
 	void DrawContent() override;
 	ImVec2 GetInitialSize() override { return ImVec2(450, 0); }
+	ImGuiWindowFlags GetWindowFlags() override { return ImGuiWindowFlags_HorizontalScrollbar; }
 private:
 	MIDIApp* app;
 
